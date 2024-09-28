a7d2d57208218c7260e31bed6f95176eb1e00d3d
davidfayanju1 <davidfayanju01@gmail.com>
2024-09-28 17:04:25 +0100

    Additions

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const [toggleNav, setToggleNav] = useState(false);
+
   return (
-    <div className="nav_container bg-white min-h-[6rem] px-[2rem] flex items-center justify-between">
+    <div className="nav_container fixed top-0 left-0 w-full z-50 bg-white min-h-[6rem] px-[2rem] flex items-center justify-between">
       <div className="item_container">
         <img src="/images/CompanyLogo.png" alt="company-logo" className="" />
       </div>
@@ -28,12 +31,38 @@ const Nav = () => {
         </ul>
       </nav>
 
+      {toggleNav && (
+        <nav className="mobile_nav_container fixed w-full pb-8 pt-5 left-0 top-[6rem] bg-white md:hidden block">
+          <ul className="">
+            <li className="flex items-start px-7 justify-center gap-4 flex-col">
+              <Link to="/" className="text-[#686868] font-semibold">
+                Business Directory
+              </Link>
+              <Link to="/" className="text-[#686868] font-semibold">
+                Features
+              </Link>
+              <Link to="/" className="text-[#686868] font-semibold">
+                Pricing
+              </Link>
+              <Link to="/" className="text-[#686868] font-semibold">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+
       <div className="burger md:hidden block">
-        <img
-          src="/svgs/burger.svg"
-          alt=""
-          //   className="w-[2rem] h-[2rem] bg-[]"
-        />
+        <button
+          onClick={() => setToggleNav(!toggleNav)}
+          className="outline-none border-none"
+        >
+          <img
+            src="/svgs/burger.svg"
+            alt=""
+            //   className="w-[2rem] h-[2rem] bg-[]"
+          />
+        </button>
       </div>
 
       <div className="button_container hidden md:flex items-center justify-center gap-7">
--- src/pages/inner/Landing.jsx
+++ src/pages/inner/Landing.jsx
@@ -4,7 +4,7 @@ import CommonLayout from "../../layout/CommonLayout";
 const Landing = () => {
   return (
     <CommonLayout>
-      <div className="hero_item_container md:flex-row flex-col md:px-10 px-4 min-h-[20rem] py-4 flex items-center justify-between">
+      <div className="hero_item_container mt-[6rem] md:flex-row flex-col md:px-10 px-4 min-h-[20rem] py-4 flex items-center justify-between">
         <header className="md:w-[45%] w-full  block">
           <h1 className="font-semibold font-serif mb-2 md:text-[2.3rem] text-[2rem]">
             List and grow your business  with just few clicks
@@ -35,7 +35,7 @@ const Landing = () => {
             <img
               src="/svgs/pic2.svg"
               alt=""
-              className="absolute md:top-[30%] top-[10%] right-[-5%]"
+              className="absolute md:top-[30%] top-[10%] right-[0%]"
             />
             <img
               src="/svgs/pic3.svg"
